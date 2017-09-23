fc27efd8697b3ee8241a60be46a763a059f30fdc
SufiyanMansoor <sufiyanbaw1256@gmail.com>
2017-09-23 21:26:44 +0500

    update code of bootstrape

---
+++ fasterbootweb/controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Sufyan on 9/23/2017.
+ */
+angular.module('starter.controllers', ['ngMaterial','ui.router'])
+
+    .controller('mainmenuCtrl', function ($scope) {
+
+    })
+    .controller('uploadproductsCtrl', function ($scope ) {
+
+    })
+    .controller('newitemsupdateCtrl', function ($scope ) {
+
+    })
+    .controller('customerordersCtrl', function ($scope ) {
+
+    });
